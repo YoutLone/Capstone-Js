394fdc244573cb6dcc1b1e190830e75b437b1cfe
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-06-28 22:04:07 +0530

    Update

--- src/js/module/homePage/homePageAPI.js
+++ src/js/module/homePage/homePageAPI.js
@@ -1,13 +1,14 @@
 import {
   generateCardHtml,
   attachCommentButtonListeners,
+  attachLikeButtonListeners,
 } from '../popup/comment.js';
 
 import { TvAPI } from '../API/API.js';
 
 const home = document.getElementById('card');
 
-const id = 6;
+const id = 6; // Set the desired range of IDs, e.g., from 1 to 10
 
 const ListApi = async () => {
   const fetchPromises = Array.from({ length: id }, (_, i) => i + 1)
@@ -29,6 +30,7 @@ const ListApi = async () => {
     );
 
     attachCommentButtonListeners();
+    attachLikeButtonListeners(); // Attach event listeners after the buttons are added to the DOM
   } catch (err) {
     throw new Error(`${err}`);
   }
