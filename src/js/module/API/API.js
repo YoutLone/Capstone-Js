e266fab18ade0a925fa5f603a3551b88731dd86e
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-06-29 19:04:54 +0530

    Update API.js

--- src/js/module/API/API.js
+++ src/js/module/API/API.js
@@ -1,5 +1,6 @@
-const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+const baseUrl =
+  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
 const TvAPI = 'https://api.tvmaze.com/people/';
-const appId = 'vOOgGfF0lcORmJGPszZP';
+const appId = 'Ky7W0EmbNf2CFFNG3oD1';
 
 export { TvAPI, baseUrl, appId };
