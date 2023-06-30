60e80d17447f18ac923151d409dde4cb22fd2272
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-06-30 18:12:26 +0530

    Commentcounter-test-pass

--- src/js/module/popup/popup.js
+++ src/js/module/popup/popup.js
@@ -1,6 +1,7 @@
 import { baseUrl, appId } from '../API/API.js';
 import submitComment from './commentForm.js';
 import generateCommentHTML from './commentshow.js';
+import getCommentsLength from './commentCounter.js';
 
 const openPopup = (data) => {
   if (!data) {
@@ -12,6 +13,8 @@ const openPopup = (data) => {
     fetch(`${baseUrl}${appId}/comments?item_id=${itemId}`)
       .then((res) => res.json())
       .then((comments) => {
+        const commentsLength = getCommentsLength(comments); // Get the comments length
+
         const uniqueComments = [
           ...new Set(comments.map((comment) => JSON.stringify(comment))),
         ].map((comment) => JSON.parse(comment));
@@ -21,29 +24,29 @@ const openPopup = (data) => {
           .join('');
 
         const commentFormHTML = `
-        <h2 class="mt-3">Add a comment</h2>
-        <form id="comment-form">
-          <input type="text" id="name-input" placeholder="Your name" required>
-          <input type="text" id="insights-input" placeholder="Your insights" required>
-          <button id="comment-button" type="submit">Comment</button>
-        </form>
-      `;
+          <h2 class="mt-3">Add a comment</h2>
+          <form id="comment-form">
+            <input type="text" id="name-input" placeholder="Your name" required>
+            <input type="text" id="insights-input" placeholder="Your insights" required>
+            <button id="comment-button" type="submit">Comment</button>
+          </form>
+        `;
 
         const popupContent = `
-        <div class="popup-content container">
-          <i class="fa-solid fa-xmark close-btn"></i>
-          <img src="${data.image.medium}" alt="#" class="popup-image">
-          <p class="popup-name">Name: ${data.name}</p>
-          <p class="popup-gender">Gender: ${data.gender}</p>
-          <p class="popup-country">Country: ${data.country.name}</p>
-          <p class="popup-timezone">Timezone: ${data.country.timezone}</p>
-        </div>
-        <h2>Comments (${uniqueComments.length})</h2>
-        <div class="comment-border border border-light border-3 rounded-5">
-        ${commentsHTML}
-        </div>
-        ${commentFormHTML}
-      `;
+          <div class="popup-content container">
+            <i class="fa-solid fa-xmark close-btn"></i>
+            <img src="${data.image.medium}" alt="#" class="popup-image">
+            <p class="popup-name">Name: ${data.name}</p>
+            <p class="popup-gender">Gender: ${data.gender}</p>
+            <p class="popup-country">Country: ${data.country.name}</p>
+            <p class="popup-timezone">Timezone: ${data.country.timezone}</p>
+          </div>
+          <h2>Comments (${commentsLength})</h2> 
+          <div class="comment-border border border-light border-3 rounded-5">
+            ${commentsHTML}
+          </div>
+          ${commentFormHTML}
+        `;
 
         const popup = document.getElementById('popup');
         popup.innerHTML = popupContent;
